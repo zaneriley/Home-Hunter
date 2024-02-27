050c794ad52ca99c186641a3ebd0f969cb2ac3dc
zaneriley <zaneriley@gmail.com>
2024-02-28 00:07:08 +0900

    working init, next is cleanup

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 from configparser import ConfigParser, SectionProxy
-from typing import Dict, List, Optional, Union
+from typing import Dict, List, Set, Optional, Union
 from abc import ABC, abstractmethod
 
 import logging
@@ -78,10 +78,11 @@ class AbstractHunter(ABC):
         Initialize the hunter with a configuration section from configparser or a dictionary.
         """
         self.config = config
-        self.seen_listings: List[str] = []
-        self.new_listings: List[str] = [] # For notification summaries
+        self.listings = {
+            "seen_listings": {},  # URL as key, listing details as value
+            "new_listings": []  # List of new listing details
+        }
         self.load_seen_listings()
-        
 
     def restart_driver(self):
         """Close and restart the WebDriver."""
@@ -112,6 +113,8 @@ class AbstractHunter(ABC):
     def ensure_storage_directory_exists(self):
         """Ensure the storage directory exists."""
         os.makedirs(self.storage_directory, exist_ok=True)
+        if not os.access(self.storage_directory, os.W_OK):
+            raise PermissionError(f"Directory {self.storage_directory} is not writable.")
         
     def save_screenshot(self, filename):
         """
@@ -141,39 +144,68 @@ class AbstractHunter(ABC):
         return os.path.join(self.storage_directory, 'seen_listings.json')
     
     def load_seen_listings(self):
-        """Load seen listings from a file."""
+        self.ensure_storage_directory_exists()
+
         try:
             with open(self.seen_listings_file, 'r') as file:
-                self.seen_listings = json.load(file)
+                self.seen_listings = json.load(file) 
+            logger.info("Loaded seen listings from file.")
         except FileNotFoundError:
-            self.seen_listings = []
-
+            self.seen_listings = {}  
+            logger.info("Seen listings file not found. Starting with an empty set.")
+        except json.JSONDecodeError:
+            self.seen_listings = {}  
+            logger.error("Error decoding the seen listings file. Starting with an empty set.")
+    
     def save_seen_listings(self):
-        """Save seen listings to a file."""
-        with open(self.seen_listings_file, 'w') as file:
-            json.dump(self.seen_listings, file)
+        try:
+            if self.listings['seen_listings']:
+                first_url, first_listing_details = next(iter(self.listings['seen_listings'].items()))
+                pretty_first_listing = json.dumps({first_url: first_listing_details}, indent=4)
+                logger.debug(f"First seen listing to be saved: {pretty_first_listing}")
+            else:
+                logger.debug("No listings to save.")
 
-    # Check if a listing has already been seen
-    def has_listing_been_seen(self, listing_url):
-        """Check if a listing has already been seen."""
-        return listing_url in self.seen_listings
+            pretty_listings = json.dumps(self.listings['seen_listings'], indent=4)
+            with open(self.seen_listings_file, 'w') as file:
+                file.write(pretty_listings)
+            
+            logger.info("Saved seen listings to file: %s", self.seen_listings_file)
+        except IOError as e:
+            logger.error(f"IOError when trying to save seen listings: {e}")
+        except Exception as e:
+            logger.error(f"Unexpected error when saving seen listings: {e}")
+    
+    def process_listings(self, scraped_listings):
+        """Process a batch of scraped listings."""
+        if scraped_listings:  # Check if there are scraped listings to process
+            # Print the header once
+            header = f"\n===== Processing Scraped Listings =====\n"
+            print(header, flush=True)
 
-    def mark_listing_seen(self, listing_url):
-        """Mark a listing as seen."""
-        if listing_url not in self.seen_listings:
-            self.seen_listings.append(listing_url)
-            self.save_seen_listings()
-            # Check if it's a new listing and add it to new_listings
-            if listing_url not in self.new_listings:
-                self.new_listings.append(listing_url)
+            # Iterate over each listing in the scraped listings
+            for listing in scraped_listings:
+                logger.info("Evaluating listing %s", listing['url'])
+                # Check if the listing URL is in the seen_listings dictionary
+                if listing['url'] not in self.seen_listings:
+                    logger.info(self.listings["seen_listings"])
+                    # If not, it's a new listing. Log it and add to new_listings and seen_listings
+                    logger.info("New listing found, adding.\n")
+                    self.listings["new_listings"].append(listing)
+                    self.listings["seen_listings"][listing['url']] = listing
+                else:
+                    # If the listing is already seen, log that it's being skipped
+                    logger.info("%s \nAlready seen, skipping.", listing['url'])
 
-    def track_new_listing(self, listing_details):
-        """Track new listings if not already seen."""
-        if not self.has_listing_been_seen(listing_details['url']):
-            self.new_listings.append(listing_details)
-            self.mark_listing_seen(listing_details['url'])
-            logger.info("Tracking new listing: %s", listing_details['url'])
+            print("\n", flush=True)
 
+        # After processing all listings, check if there are new listings to announce
+        if self.listings["new_listings"]:
+            self.announce_new_listings()
+
+    
+        self.listings["new_listings"] = []
+    
     @abstractmethod
     def format_listing_message(self, listing_details):
         """
@@ -186,52 +218,40 @@ class AbstractHunter(ABC):
 
     def announce_new_listings(self):
         """Announce new listings based on their count."""
-        total_new_listings = len(self.new_listings)
-        logger.info("Announcing new listings, count: %d", len(self.new_listings))
+        new_listings_count = len(self.listings["new_listings"])
 
-        if total_new_listings == 0:
-            return  # No new listings to announce
+        if new_listings_count == 0:
+            logger.info("No new listings found, skipping notification.")
+            return
 
-        if total_new_listings < 3:
-            # Send individual notifications with custom embeds for each listing
-            for listing_details in self.new_listings:
-                embed_payload = self.format_listing_message(listing_details)
-                self.send_notification(embed_payload)
+        if new_listings_count < 3:
+            for listing in self.listings["new_listings"]:
+                logger.info("Preparing message for %s", listing['url'])
+                self.send_notification(self.format_listing_message(listing))
         else:
-            # For three or more listings, prepare a summary notification
-            # and include embeds for the first three listings
-            content = f"Found {total_new_listings} new listings. Check them out here: [View Listings](YourListingsPageURL)"
-            embeds = [self.format_listing_message(listing_details) for listing_details in self.new_listings[:3]]
-            embed_payload = {"content": content, "embeds": embeds}
-            logger.info(f"Sending notification: {embed_payload}")
-            self.send_notification(embed_payload)
+            logger.info("Preparing summary for %d listings", len(self.listings["new_listings"]))
+            self.send_summary_notification(self.listings["new_listings"][:3])
 
-        self.new_listings = []
+        self.listings["new_listings"] = []  
 
     def send_notification(self, embed_payload):
-        """
-        Send a notification about a new listing.
-        """
-        enable_notifications = self.config.get("enable_notifications", False)
-        notification_url = self.config.get("notification_url", os.getenv("NOTIFICATION_URL"))
-
-        if enable_notifications and notification_url:
-            logger.info("Attempting to send notification. Payload: %s", embed_payload)
-            try:
-                response = requests.post(notification_url, json=embed_payload, timeout=10)
-                response.raise_for_status()  # Raises an exception for 4XX/5XX errors
-            except requests.exceptions.HTTPError as http_err:
-                logger.error(f"HTTP error occurred: {http_err}; Response body: {response.text}")
-            except requests.exceptions.ConnectionError as conn_err:
-                logger.error(f"Connection error occurred: {conn_err}")
-            except requests.exceptions.Timeout as timeout_err:
-                logger.error(f"Timeout error occurred: {timeout_err}")
-            except requests.exceptions.RequestException as req_err:
-                logger.error(f"Request exception occurred: {req_err}")
+        """Send a notification with the given payload."""
+        if self.config.get("enable_notifications", False):
+            response = requests.post(self.config["notification_url"], json=embed_payload)
+            if response.status_code == 200:
+                logger.info("Notification sent successfully.")
             else:
-                logger.info(f"Successfully sent notification to Discord; Status Code: {response.status_code}")
+                logger.error("Failed to send notification.")
         else:
-            pass
+            logger.info("Notifications are disabled, skipping notification.")
+            logger.info("Would have sent notification: %s", embed_payload)
+            logger.info("⚠️  Edit the docker-compose file to enable notifications.  ⚠️ ")
+
+    def send_summary_notification(self, listings):
+        """Send a summary notification for multiple listings."""
+        content = f"Found {len(listings)} new listings. Check them out here: [View Listings](YourListingsPageURL)"
+        embeds = [self.format_listing_message(listing) for listing in listings]
+        self.send_notification({"content": content, "embeds": embeds})
 
 class SUUMOHunter(AbstractHunter, WebDriverBase):
     def __init__(self):
@@ -268,7 +288,7 @@ class SUUMOHunter(AbstractHunter, WebDriverBase):
             logger.info("Clicked on list view button.")
 
             WebDriverWait(self.driver, self.config["dynamic_content_timeout"]).until(
-                lambda d: len(d.find_elements(By.CSS_SELECTOR, "ul.listView.bukkenList.solid > li")) > 0
+                lambda d: len(d.find_elements(By.CSS_SELECTOR, "ul.listView.bukkenList > li")) > 0
             )
             logger.info("Listings have successfully loaded.")
             self.save_screenshot('screenshot_after_button_click.png')  
@@ -276,12 +296,13 @@ class SUUMOHunter(AbstractHunter, WebDriverBase):
 
             logger.info("Dynamic content loaded")
 
-            # Find all listings
-            listings = self.driver.find_elements(By.CSS_SELECTOR, "li")  
-            logger.info("Found %d listings", len(listings))
+            all_listings = []
 
+            # Scrape the website for listings
+            listings = self.driver.find_elements(By.CSS_SELECTOR, "ul.listView.bukkenList > li")
+
+            # Iterate over each listing and extract details
             for listing in listings:
-                logger.info("Processing listing %s", listing.text)
                 try:
                     price = listing.find_element(By.CSS_SELECTOR, ".price").text
                     size = listing.find_element(By.CSS_SELECTOR, ".exclusive").text
@@ -298,39 +319,27 @@ class SUUMOHunter(AbstractHunter, WebDriverBase):
                         "url": url,
                         "image_url": image_url
                     }
-                    
-                    if not self.has_listing_been_seen(listing_details['url']):
-                        embed_payload = self.format_listing_message(listing_details)
-                        self.mark_listing_seen(listing_details['url'])
-                        self.track_new_listing(listing_details)
-                        
+        
+                    # Append the extracted details to the all_listings list
+                    all_listings.append(listing_details)
 
-                    else:
-                        logger.info(f"Listing already seen: {price}")
                 except NoSuchElementException as e:
+                    # Log any errors encountered during extraction
                     logger.error(f"Error extracting details for listing: {e}")
+
+            # Pass the scraped listings to the abstract class's method for further processing
+            self.process_listings(all_listings)
+            # Use the announce_new_listings method to handle notification logic
             self.announce_new_listings()
+
         except TimeoutException as e:
             logger.error(f"Timeout waiting for content: {e}")
         
-        
-
-
         finally:
             for entry in self.driver.get_log('browser'):
                 logger.info(entry)
             self.close_driver()
             logger.info("Driver closed")
-    
-    def has_listing_been_seen(self, listing_url):
-        # Check if a listing has already been seen
-        return listing_url in self.seen_listings
-    
-    def mark_listing_seen(self, listing_url):
-        # Mark a listing as seen
-        if listing_url not in self.seen_listings:
-            self.seen_listings.append(listing_url)
-            self.save_seen_listings()
 
     def format_listing_message(self, listing_details):
         embed_payload = {
@@ -364,13 +373,13 @@ if __name__ == "__main__":
     green = '\033[92m'
     red = '\033[91m'
     ascii_logo = blue_bold + """
-ooooo   ooooo                                       ooooo   ooooo                             .                      
-`888'   `888'                                       `888'   `888'                           .o8                      
- 888     888   .ooooo.  ooo. .oo.  .oo.    .ooooo.   888     888  oooo  oooo  ooo. .oo.   .o888oo  .ooooo.  oooo d8b 
- 888ooooo888  d88' `88b `888P"Y88bP"Y88b  d88' `88b  888ooooo888  `888  `888  `888P"Y88b    888   d88' `88b `888""8P 
- 888     888  888   888  888   888   888  888ooo888  888     888   888   888   888   888    888   888ooo888  888     
- 888     888  888   888  888   888   888  888    .o  888     888   888   888   888   888    888 . 888    .o  888     
-o888o   o888o `Y8bod8P' o888o o888o o888o `Y8bod8P' o888o   o888o  `V88V"V8P' o888o o888o   "888" `Y8bod8P' d888b                                                                                                                                                                                                                                                                                      
+ooooo   ooooo                                   ooooo   ooooo                         .                      
+`888'   `888'                                   `888'   `888'                       .o8                     
+ 888     888  .ooooo. ooo. .oo.  .oo.   .ooooo.  888     888 oooo  oooo ooo. .oo. .o888oo  .ooooo.  oooo d8b
+ 888ooooo888 d88' `88b`888P"Y88bP"Y88b d88' `88b 888ooooo888 `888  `888 `888P"Y88b  888   d88' `88b `888""8P
+ 888     888 888   888 888   888   888 888ooo888 888     888  888   888  888   888  888   888ooo888  888
+ 888     888 888   888 888   888   888 888    .o 888     888  888   888  888   888  888 . 888    .o  888
+o888o   o888o`Y8bod8P o888o o888o o888o`Y8bod8P'o888o   o888o `V88V"V8P'o888o o888o "888" `Y8bod8P' d888b                                                                                                                                                                                                                                                                                      
     """ + reset
     print(ascii_logo, flush=True)
     logger.info("Starting home-hunter")
@@ -382,7 +391,7 @@ o888o   o888o `Y8bod8P' o888o o888o o888o `Y8bod8P' o888o   o888o  `V88V"V8P' o8
         # Notification disabled message
         alert_message = f"""{yellow}
             ⚠️  Attention: Notifications are disabled or notification URL is not provided. ⚠️
-            ⚠️                      Notifications will NOT be sent.                        ⚠️
+                                   Notifications will NOT be sent.                        
                         {reset}"""
         print(alert_message, flush=True)
     
